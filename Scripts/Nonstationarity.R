c9351ee3927e17afae725856a45813f71de8d91f
PCarr97 <patrick.timothy.carr@gmail.com>
2023-02-27 17:29:22 -0500

    Added augmented Dickey-Fuller test

--- Scripts/Nonstationarity.R
+++ Scripts/Nonstationarity.R
@@ -3,3 +3,8 @@
 # Autocorrelation
 dynamic_model <- dynlm(wages ~ fed_rate, data = data)
 acf(residuals(dynamic_model))
+
+# Augmented Dickey-Fuller Test
+summary(ur.df(data$wages, type = "drift", lags = 0))
+
+# KPSS Test
