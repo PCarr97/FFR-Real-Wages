f94db8e4a4fdf90dab23386f14c897196a40f541
PCarr97 <patrick.timothy.carr@gmail.com>
2023-02-27 16:45:06 -0500

    Percent format for FFR variable

--- Scripts/Data Import.R
+++ Scripts/Data Import.R
@@ -1,10 +1,12 @@
 install.packages('pacman')
 library(pacman)
-p_load(tidyverse, janitor, lubridate, sqldf)
+p_load(tidyverse, janitor, lubridate, sqldf, scales, ggplot)
 
 #Import Federal Funds Rate Data
 ffr <- read_csv("Raw Data/FEDFUNDS.csv",
                 col_names = c("date", "fed_rate"), skip = 1)
+ffr$fed_rate <- as.numeric(ffr$fed_rate / 100)
+
 
 #Import Wage Data
 wages <- read_csv("Raw Data/LES1252881600Q.csv",
@@ -14,3 +16,5 @@ data <- sqldf (
   "select w.date, w.wages, f.fed_rate from wages w
   left join ffr f on w.date = f.date"
 )
+
+view(data)
