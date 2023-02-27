41c2ea905b2f8e754ab0ce072447d0123fff84d4
PCarr97 <patrick.timothy.carr@gmail.com>
2023-02-27 18:26:54 -0500

    Added lmtest and xts

--- Scripts/Package Uploads.R
+++ Scripts/Package Uploads.R
@@ -1,3 +1,3 @@
 install.packages('pacman')
 library(pacman)
-p_load(tidyverse, janitor, lubridate, sqldf, scales, ggplot, dynlm, urca, tseries)
+p_load(tidyverse, janitor, lubridate, sqldf, scales, ggplot, dynlm, urca, tseries, lmtest, xts)
