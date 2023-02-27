5bc7b5ebc90f0056558233a72299110a964ab0f7
PCarr97 <patrick.timothy.carr@gmail.com>
2023-02-27 16:11:26 -0500

    Initial project creation; data import

---
+++ Data Import.R
@@ -0,0 +1,16 @@
+install.packages('pacman')
+library(pacman)
+p_load(tidyverse, janitor, lubridate, sqldf)
+
+#Import Federal Funds Rate Data
+ffr <- read_csv("Raw Data/FEDFUNDS.csv",
+                col_names = c("date", "fed_rate"), skip = 1)
+
+#Import Wage Data
+wages <- read_csv("Raw Data/LES1252881600Q.csv",
+                  col_names = c("date", "wages"), skip = 1)
+
+data <- sqldf (
+  "select w.date, w.wages, f.fed_rate from wages w
+  left join ffr f on w.date = f.date"
+)
